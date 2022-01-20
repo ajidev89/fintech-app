b5bc73df6b2ec3bd1915c5bba517f460065d7290
mazeeer <ajidagba21@gmail.com>
2022-01-20 05:25:53 +0100

    first commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,24 @@
+<header class="lg:container lg:mx-auto mx-3 py-5 lg:py-8" >
+    <div class="flex items-center justify-between">
+        <div class="font-semibold text-brand text-xl" >Hallifield</div>
+        <div class="lg:flex space-x-6 hidden" >
+            <ul class="flex items-center space-x-10 ">
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>FAQs</li>
+            </ul>
+            <button class="btn">
+                Log in
+            </button>
+            <button class="btn">
+                Sign up
+            </button>
+        </div>
+        <div class="lg:hidden" >
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-brand" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+        </div>
+    </div>    
+</header>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,42 @@
+@extends('index')
+
+@section('content')
+    <section class="lg:container lg:mx-auto mx-3 py-5 lg:py-8 grid grid-cols-1 lg:grid-cols-2 place-c lg:gap-4" >
+        <div class="flex items-center mb-8 lg:mb-0">
+            <div>
+                <h1 class="text-3xl lg:text-5xl font-semibold leading-tight ">
+                    Send money <br> 
+                    to your family or friend <br>
+                    in different countries.
+                </h1>
+                <div class="mt-4">
+                    <button class="btn-big mr-4">
+                        Log in
+                    </button>
+                    <button class="btn-big">
+                        Sign up
+                    </button>
+                </div>
+
+            </div>  
+        </div>
+        <div class="" >
+            <div class="bg-white shadow-2xl rounded-2xl p-8" >
+                <p class="text-xl font-semibold" >Send Money</p>
+                <form action="" method="post" class="mt-6">
+                    <select name="" id="" class="hm-input">
+                        <option value="NG">Nigeria</option>
+                    </select>
+                    <select name="" id="" class="hm-input mt-4">
+                        <option value="NG">Select Delivery Method</option>
+                    </select>
+                    <input type="number" class="hm-input mt-4" placeholder="Amount to Send">
+                    <input type="number" class="hm-input mt-4" placeholder="Amount to Send">
+                    <input type="submit" class="hm-send mt-4" value="Send">
+                </form>
+            </div>
+        
+        </div>
+
+    </section>
+@endsection
