db84c2a6f636cb5f0f1de243e39af0cf586b3c9e
mazeeer <ajidagba21@gmail.com>
2022-01-21 09:31:37 +0100

    SSL

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,15 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['middleware' => 'ForceSSL'], function(){
 
-    Route::get('/', 'App\Http\Controllers\WebController@index')->name('home');
-    Route::get('/about', 'App\Http\Controllers\WebController@about')->name('about');
-    Route::get('/contact', 'App\Http\Controllers\WebController@contact')->name('contact');
-    Route::get('/faqs', 'App\Http\Controllers\WebController@faqs')->name('faqs');
+Route::get('/', 'App\Http\Controllers\WebController@index')->name('home');
+Route::get('/about', 'App\Http\Controllers\WebController@about')->name('about');
+Route::get('/contact', 'App\Http\Controllers\WebController@contact')->name('contact');
+Route::get('/faqs', 'App\Http\Controllers\WebController@faqs')->name('faqs');
 
 
-    Route::get('/login', 'App\Http\Controllers\UserController@showLogin')->name('login');
-    Route::get('/sign-up', 'App\Http\Controllers\UserController@showSignUp')->name('signUp');
-
-});
+Route::get('/login', 'App\Http\Controllers\UserController@showLogin')->name('login');
+Route::get('/sign-up', 'App\Http\Controllers\UserController@showSignUp')->name('signUp');
